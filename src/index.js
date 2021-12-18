cd56c1802e3eea60b32fb5c497ba20f9d190c8c1
Larissa Lopes <larissasantos.lopes@hotmail.com>
2021-12-18 01:29:29 -0300

    Adicionada funcionalidade no botão 'Adicionar novo' que faz a seção do formulario aparecer ao clicar

--- src/index.js
+++ src/index.js
@@ -19,3 +19,19 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+// Botão adicionar novo - aparecimento do formulario
+let btnAddNovo = document.querySelector(".btn-primary")
+
+btnAddNovo.addEventListener("click" , 
+function() {
+    let addCurso = document.querySelector(".addCurso");
+
+    if(addCurso.style.display === "block") {
+        addCurso.style.display = "none";
+    } else {
+        addCurso.style.display = "block";
+    }
+
+})
